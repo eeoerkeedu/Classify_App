d89c2c7fdb9c4c9f18d703401d4db70f1c9ad5ba
LilTzar <eeoerkeedu@gmail.com>
2022-12-07 11:16:12 -0700

    heroku deploy again

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-	process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/classify-app",
-	{
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	}
-);
+mongoose
+	.connect(
+		process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/classify-app",
+		{
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		}
+	)
+	.then(() => console.log("MongoDB has been connected"))
+	.catch((err) => console.log(err));
 
 module.exports = mongoose.connection;
